d066e58ccd9fcf2492cdf91d81f43189b59d5137
Leela-369 <gonnabathulaleelasanthosh@gmail.com>
2023-03-14 17:51:10 +0530

    not completed

--- src/component/About.js
+++ src/component/About.js
@@ -1,10 +1,43 @@
 import React from 'react'
+import { Typography } from '@mui/material'
+import mark from '../assets/mark.png'
+import about from '../assets/about.png'
+import './About.css'
 
 export const About = () => {
   return (
+    <>
+    <Typography className='heading' sx={{display:{xs:"block",sm:"block",md:"none"}}}>
+        Resume Builder
+      </Typography>
+      
+      <Typography className='heading' sx={{display:{xs:"none",sm:"none",md:"block"}}}>
+        Resume<br/>Builder
+      </Typography>
+      <img src={mark}  alt='' className='image1'/>
+      <p className='content' >
+            A resume builder is a great tool that enables people to create a professional and visually appealing resume.
+            These tools help job seekers to structure their information and present it in an organized,
+            efficient and attractive manner. Using a resume builder saves time and effort and ensures that the final 
+            result is an impressive and effective resume that can help you land that dream job. 
+            Overall, a resume builder is one of the best tools for anyone looking 
+            for a hassle-free way to create an excellent resume.Choose from several template options and even
+            pre-populate a resume from your LinkedIn profile. Create an account
+            to save your progress and multiple versions, plus download as a
+            PDF.Resume Builder offers free, HR-approved resume templates to help
+            you create a professional resume in minutes. Choose from several
+            template options and even pre-populate a resume from your LinkedIn
+            profile. Create an account to save your progress and multiple
+            versions, plus download as a PDF.
+      </p>
+      <img src={about} alt='' className='image2'/>
+      <p className='share'>
+        Share with  your friends
+      </p>
+      
     
-    <p>
-        Leela santhosh
-    </p>
+
+    
+    </>
   )
 }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,47 +1,104 @@
 import React from 'react'
 import AppBar from '@mui/material/AppBar'
+import { Drawer,  List, Typography } from '@mui/material'
+import {IconButton} from '@mui/material'
+import Divider from '@mui/material/Divider'
 import { NavLink } from 'react-router-dom'
-import { Box} from '@mui/system'
-// import { Typography } from '@mui/material'
-  import logo from '../assets/alma2.png'
+import { Box, Stack} from '@mui/system'
+import MenuIcon from '@mui/icons-material/Menu';
+import Almabetterlogo from '../assets/almabetterlogo.png'
+import './Navbar.css'
 
 
+const drawerWidth = 240;
 
+export const Navbar = (props) => {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
-export const Navbar = () => {
-  const navLinkStyles = ({isActive}) => {
-    return{
-      textDecoration: isActive ? 'none': 'none',
-      color: isActive ? 'rgb(0, 98, 255)' : 'Black',
+  const handleDrawerToggle = () => {
+    setMobileOpen (!mobileOpen);
+  };
+  const drawer =(
+    <Box onClick ={handleDrawerToggle} sx={{textAlign:"center"}}>
+      <Typography variant='h5' sx={{my:'2'}}>
+      <img  src={Almabetterlogo} alt='logo'  height={37}/>
+      </Typography>
+      <Divider/>
+      <List
+      className='drawerLinks'
+      sx={{
+        display: "flex",
+        textAlign: "left",
+        paddingLeft: "20px",
+        flexDirection: "column",
+        textDecoration:"none",
+        color:"black",
+        '&:hover':{
+          color:' rgb(0, 128, 255)'
+        }
       
-    }
-  }
+      }}>
+        <NavLink to='/'  className="navLinks">
+           Templates </NavLink>
+        <NavLink to='/myresumes'  className="navLinks">
+           My Resumes </NavLink>
+        <NavLink to='/about'  className="navLinks" >
+           About </NavLink>
+      </List>
+    </Box>
+  )
 
   return (
-    <AppBar sx={{backgroundColor:'white' , color: 'black'}}>
-     
-      <Box
-      display={'flex'}
-      flexDirection={'row'}
-      fontSize={'22px'}
-      gap={'30px'}
-      padding={'15px'}
-      marginRight={'30px' }
-      
-      
-      >
-        <img  src={logo} alt='logo'  height={37}/>
-        {/* <Typography marginRight={'auto'} fontSize={'22px'} padding={'10px'}marginLeft={'20px'} display={'inline-flex'}><span style={{backgroundColor:'#d50000', color:'white', borderRadius:'15%', padding:'1%'  ,fontWeight:'bolder',}}>Al</span> <span> maBetter </span></Typography> */}
-        
-        <NavLink to='/' style={navLinkStyles}  className='navLink'>
+    <>
+      <AppBar  component="nav" sx={{backgroundColor:'white' , color: 'black',padding:{xs:'3px',sm:'10px'}}} className="appbar">
+       <Box display={'flex'}
+       flexDirection={'row'}
+       alignItems={'center'}
+       marginRight={'30px'}
+       >
+       <IconButton
+       size="large"
+       color="inherit"
+       edge="end"
+       id="icon"
+       onClick={handleDrawerToggle}
+       sx={{mr:2, display:{sm:"none"}}}>
+         <MenuIcon sx={{ fontSize:'40px','&:hover':{border:'1px solid #b5b5b5'},borderRadius:'50%',padding:'3px'}}/>
+         </IconButton>
+          <img  src={Almabetterlogo} alt='logo'  height={37}/>
+                    <Box sx={{display:{xs:'none',sm:'block'}}}>
+             <Stack direction={'row'} spacing={6}>
+           <NavLink to='/'  className="navLink">
            Templates </NavLink>
-        <NavLink to='/myresumes' style={navLinkStyles} marginLeft={'auto'} className='navLink'>
+            <NavLink to='/myresumes'  className="navLink">
            My Resumes </NavLink>
-        <NavLink to='/about' style={navLinkStyles}marginLeft={'auto'} className='navLink' >
+           <NavLink to='/about'  className="navLink" >
            About </NavLink>
+           </Stack>
+           </Box>
+           </Box>
+       </AppBar>
+    
+       <Box component="nav">
+        <Drawer
+       variant="temporary"
+       open={mobileOpen}
+       onClose = {handleDrawerToggle}
+       ModalProps={{
+        keepMounted: true,
+       }}
+       sx={{
+          display: {xs: "block",sm: "none"},
+         "& .MuiDrawer-paper": {
+          boxSizing: "border-box",
+          width: drawerWidth,
+          }
+       }}>
+        {drawer}
+       </Drawer>
         
-        </Box>
-        
-    </AppBar>
+    </Box>
+    </>
+
   )
 }
