e4a82f5d119da4ec17a1c009a706d88c4b2aca41
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-03-20 13:14:12 +0530

    added some pages to Browser router

--- src/pages/DetailsFillingPage.jsx
+++ src/pages/DetailsFillingPage.jsx
@@ -1,10 +1,11 @@
 import { Box, Paper, Tabs, Tab } from '@mui/material'
 import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import React,{ useState } from 'react'
+import React, { useState } from 'react'
 import EducationInfo from '../form_components/EducationInfo'
 import PersonalInfo from '../form_components/PersonalInfo'
 import WorkExperience from '../form_components/WorkExperience';
+import Layout from '../Layout/Layout';
 
 
 const DetailsFillingPage = () => {
@@ -15,61 +16,64 @@ const DetailsFillingPage = () => {
   };
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-<Box sx={{
-  display:'flex',
-  flexDirection:'row',
-  justifyContent:'space-evenly'
-}}>
-<Box>
-<Tabs
-          value={tabIndex}
-          onChange={handleTabChange}
+    <Layout>
 
-          orientation="vertical"
-        >
-          <Tab label="Personal Information" />
-          <Tab label="Work Experience" />
-          <Tab label="Education Information" />
-        </Tabs>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Box sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-evenly'
+        }}>
+          <Box>
+            <Tabs
+              value={tabIndex}
+              onChange={handleTabChange}
+
+              orientation="vertical"
+            >
+              <Tab label="Personal Information" />
+              <Tab label="Work Experience" />
+              <Tab label="Education Information" />
+            </Tabs>
+          </Box>
+
+
+          {tabIndex === 0 && (
+            <Paper elevation={5} sx={{
+              padding: '25px',
+
+            }} >
+              <PersonalInfo />
+
+            </Paper>
+          )}
+          {tabIndex === 1 && (
+            <Paper elevation={5} sx={{
+              padding: '25px',
+
+            }} >
+              <WorkExperience />
+
+            </Paper>
+          )}
+          {tabIndex === 2 && (
+            <Paper elevation={5} sx={{
+              padding: '25px',
+
+            }} >
+              <EducationInfo />
+
+            </Paper>
+          )}
+
+
+
+
+
         </Box>
-   
 
-       {tabIndex === 0 && (
-             <Paper elevation={5} sx={{
-        padding:'25px',
-        
-    }} >
-        <PersonalInfo   /> 
-       
-        </Paper>
-          )}
-           {tabIndex === 1 && (
-             <Paper elevation={5} sx={{
-        padding:'25px',
-        
-    }} >
-        <WorkExperience/>
-       
-        </Paper>
-          )}
-           {tabIndex === 2 && (
-             <Paper elevation={5} sx={{
-        padding:'25px',
-        
-    }} >
-          <EducationInfo/>
-       
-        </Paper>
-          )}
-       
-     
-       
-       
-      
-    </Box>
-    
-    </LocalizationProvider>
+      </LocalizationProvider>
+    </Layout>
   )
 }
 
