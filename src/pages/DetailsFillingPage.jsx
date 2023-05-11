dea89061d5ae03894dd1fbb24d1ce6d0aaaf266f
Leela-369 <gonnabathulaleelasanthosh@gmail.com>
2023-05-11 19:29:26 +0530

    completed

--- src/pages/DetailsFillingPage.jsx
+++ src/pages/DetailsFillingPage.jsx
@@ -1,91 +1,38 @@
 import { Box, Paper, Tabs, Tab } from '@mui/material'
-import { LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+// import { LocalizationProvider } from '@mui/x-date-pickers'
+// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import React,{ useState } from 'react'
 import EducationInfo from '../features/educationInformation/EducationInformation'
 import PersonalInfo from '../form_components/PersonalInfo'
 import WorkExperience from '../features/workExperience/WorkExperience';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+// import IconButton from '@mui/material/IconButton';
+// import MenuIcon from '@mui/icons-material/Menu';
 // import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 // import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import Drawer from '@mui/material/Drawer';
-import {styled,useTheme} from '@mui/material/styles';
+// import Drawer from '@mui/material/Drawer';
+// import {styled,useTheme} from '@mui/material/styles';
 
-const drawerWidth = 340;
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: `-${drawerWidth}px`,
-    ...(open && {
-      transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      marginLeft: 0,
-    }),
-  }),
-);
+
+
 
 
 const DetailsFillingPage = () => {
+ const [tabIndex, setTabIndex] = useState(0);
 
- const theme = useTheme();
-  const [tabIndex, setTabIndex] = useState(0);
-  const [open,setOpen] = useState(false);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
 
   const handleTabChange = (event, newTabIndex) => {
     setTabIndex(newTabIndex);
   };
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
+    
 <Box sx={{
   display:'flex',
   flexDirection:'row',
   justifyContent:'space-evenly'
 }}>
-  <Main>
- <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="persistent"
-        anchor="left"
-        open={open}
-      >
-      
-          {/* <IconButton onClick={handleDrawerClose}>
-          <ChevronLeftIcon/>
-          </IconButton> */}
-       
-<IconButton
-            color="red"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-           
-            sx={{ mr: 2, ...(open && { display: 'none' }) }}
-          >
-            <MenuIcon />
-          </IconButton>
+  
+ 
          
 <Tabs
           value={tabIndex}
@@ -97,7 +44,7 @@ const DetailsFillingPage = () => {
           <Tab label="Work Experience" />
           <Tab label="Education Information" />
         </Tabs>
-        </Drawer>
+        
 
        {tabIndex === 0 && (
              <Paper elevation={5} sx={{
@@ -129,11 +76,10 @@ const DetailsFillingPage = () => {
        
      
        
-       </Main>
+      
       
     </Box>
     
-    </LocalizationProvider>
   )
 }
 
