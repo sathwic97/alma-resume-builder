69e781dc682f053fa57b48212b71b5878619945e
Sathwic97 <sathwic97@gmail.com>
2023-05-14 15:15:11 +0530

    resolved errors

--- src/pages/DetailsFillingPage.jsx
+++ src/pages/DetailsFillingPage.jsx
@@ -1,89 +1,58 @@
-<<<<<<< HEAD
-import { Box, Paper, Tabs, Tab } from '@mui/material'
-// import { LocalizationProvider } from '@mui/x-date-pickers'
-// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import React,{ useState } from 'react'
-=======
 import { Box, Tabs, Tab, Typography } from '@mui/material'
 import React from 'react'
->>>>>>> 194c82670ae621f02844461ff77469f07ddc2f05
 import EducationInformation from '../features/educationInformation/EducationInformation'
 import PersonalInformation from '../features/personalInformation/PersonalInformation'
 import WorkExperience from '../features/workExperience/WorkExperience';
-// import IconButton from '@mui/material/IconButton';
-// import MenuIcon from '@mui/icons-material/Menu';
-// import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-// import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-// import Drawer from '@mui/material/Drawer';
-// import {styled,useTheme} from '@mui/material/styles';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import Layout from '../layout/Layout';
-<<<<<<< HEAD
-=======
 import PropTypes from 'prop-types';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { useSelector } from 'react-redux';
->>>>>>> 194c82670ae621f02844461ff77469f07ddc2f05
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
 
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
 
 
 const DetailsFillingPage = () => {
-<<<<<<< HEAD
- const [tabIndex, setTabIndex] = useState(0);
-
-
-
-
-
-  const handleTabChange = (event, newTabIndex) => {
-    setTabIndex(newTabIndex);
-  };
-=======
   const value = useSelector((state)=> state.tabIndex.index)
 
   // const handleChange = (event, newValue) => {
   //   setValue(newValue);
   // };
->>>>>>> 194c82670ae621f02844461ff77469f07ddc2f05
 
   return (
-    
-    <LocalizationProvider>
+  <LocalizationProvider dateAdapter={AdapterDayjs}>
     <Layout>
-<<<<<<< HEAD
-      <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly' }}>
-
-      {/* <Tabs value={tabIndex} onChange={handleTabChange} orientation="horizontal">
-          <Tab label="Personal Information" />
-          <Tab label="Work Experience" />
-          <Tab label="Education Information" />
-        </Tabs> */}
-
-        <Tabs value={tabIndex} onChange={handleTabChange} orientation="vertical">
-          <Tab label="Personal Information" />
-          <Tab label="Work Experience" />
-          <Tab label="Education Information" />
-        </Tabs>
-
-        <Box style={{marginLeft:'auto', marginRight:'auto'}}>
-          <Paper elevation={5} sx={{padding:'25px'}}>
-        {tabIndex === 0 && (
-          <PersonalInformation />
-        )}
-
-        {tabIndex === 1 && (
-          <WorkExperience />
-        )}
-
-        {tabIndex === 2 && (
-            <EducationInformation />
-        )}
-          </Paper>
-        </Box>
-=======
     <Box
       sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 224 }}
     >
@@ -111,10 +80,10 @@ const DetailsFillingPage = () => {
       <TabPanel value={value} index={3}>
         Item Four
       </TabPanel>
->>>>>>> 194c82670ae621f02844461ff77469f07ddc2f05
       </Box>
     </Layout>
-  </LocalizationProvider>
+    </LocalizationProvider>
+ 
   )
 }
 
